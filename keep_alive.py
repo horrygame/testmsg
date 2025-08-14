ed5d6c9385f0af060e716f58073ee2f94320e1b2
horrygame <fortmark70@gmail.com>
2025-08-14 17:26:02 +0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,10 +1,1 @@
-import requests
-import time
 
-while True:
-    try:
-        requests.get("https://testmsg-562i.onrender.com")
-        print("Server pinged")
-    except:
-        pass
-    time.sleep(300)  # Каждые 5 минут
